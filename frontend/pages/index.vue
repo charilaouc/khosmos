f10817167c53594329d00ed0eda9fb01b3702ac9
charilaouc <charilaou.c@gmail.com>
2020-02-24 12:17:46 +0200

    prepare

--- frontend/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <img src="https://media.giphy.com/media/zBL9j9oiR3VM4/giphy.gif" class="uk-position-center" alt="">
-  </div>
-</template>
--- frontend/utils/Strapi.js
+++
@@ -1,7 +0,0 @@
-import Strapi from 'strapi-sdk-javascript/build/main'
-
-const apiUrl = process.env.API_URL || 'http://167.172.186.130:1337/'
-const strapi = new Strapi(apiUrl)
-
-export default strapi;
-export { apiUrl }
